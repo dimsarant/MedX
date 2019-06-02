7c6bcc39381f20d22c88a4922b57433d13ad14dc
Dimitris Sarantakis <sarantakis_dimitris@hotmail.com>
2019-06-02 12:55:08 +0300

    latest_changes

--- ConnectDb.java
+++ ConnectDb.java
@@ -3,17 +3,14 @@ package MedX;
 import java.sql.*;
 import javax.swing.JOptionPane;
 
-public class ConnectDb {
-    Connection conn=null;   
+public class ConnectDb {  
     public static Connection ConnectDb() { 
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn=DriverManager.getConnection("jdbc:mysql://141.237.39.109:3306/medx?characterEncoding=utf8", "medx", "1234");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3308/medx?characterEncoding=utf8", "medx", "70567914");
             return conn;
-           }
-        catch(Exception e){
-            JOptionPane.showMessageDialog(null,"Database connection not established\n"+e);
-            return null;}
+        }catch(Exception e){JOptionPane.showMessageDialog(null,e);
+                            return null;
+        }
     }
-    
 }
